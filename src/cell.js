35b0bd49487124a571b66c552ed8ba9bd5e05636
Paul Bevis <paulbevis2010@gmail.com>
2016-11-15 21:57:50 +1000

    added cell styling

--- src/cell.js
+++ src/cell.js
@@ -5,15 +5,23 @@ import {graphql} from 'react-apollo';
 // A mutation is made available on a callback called `mutate`
 // Other props of the wrapping component are passed through.
 function Cell({mutate, cell, playerValue, gameBoardId}) {
-  console.log('cell/playerValue: ', cell)
+  let styling = {
+    background: 'lightGrey',
+    display: 'flex',
+    margin: '2px',
+    width: '50px',
+    height: '50px',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '40px'
+  };
   return (
-    <button onClick={() => mutate({variables: {playerValue, cellId: cell.id, gameBoardId: gameBoardId}})}>
-      {cell.value}
-    </button>
+    <div style={styling} onClick={() => mutate({variables: {playerValue, cellId: cell.id, gameBoardId: gameBoardId}})}>
+      <span style={{display: 'flex'}}>{cell.value}</span>
+    </div>
   )
 }
 
-// You can also use `graphql` for GraphQL mutations
 export default graphql(gql`
     mutation selectCell($playerValue: String!, $cellId: Int!, $gameBoardId: Int!) {
         selectCell(playerValue: $playerValue, cellId: $cellId, gameBoardId: $gameBoardId) {
