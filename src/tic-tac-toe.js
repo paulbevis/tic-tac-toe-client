461df789f5222528ef3a6448724db915dc29dc73
Paul Bevis <paulbevis2010@gmail.com>
2016-11-15 22:03:04 +1000

    using a hash of the player name for the player id

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -5,6 +5,17 @@ import {graphql, withApollo} from 'react-apollo';
 import CreateGame from './create-game'
 import RegisterPlayer from './register-player'
 import GameBoard from './game-board'
+/*eslint no-extend-native: ["error", { "exceptions": ["String"] }]*/
+String.prototype.hashCode = function() {
+  var hash = 0, i, chr, len;
+  if (this.length === 0) return hash;
+  for (i = 0, len = this.length; i < len; i++) {
+    chr = this.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
 
 class TicTacToe extends Component {
 
